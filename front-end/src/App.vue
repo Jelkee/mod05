6546411d8d3701fd852940edb9d13b8a6d860e3f
Niek <n.zieverink@student.utwente.nl>
2021-10-12 15:56:10 +0200

    Added some basic components for the home page

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div></div>
+  <q-layout view="lHh lpR lFf"> </q-layout>
 </template>
 
 <script>
+import TopBar from "./components/TopBar.vue";
+import MainPage from "./components/MainPage.vue";
+import SideBar from "./components/SideBar.vue";
 export default {
   name: "LayoutDefault",
 
-  components: {},
+  components: { SideBar, TopBar, MainPage },
 
   setup() {
     return {};
