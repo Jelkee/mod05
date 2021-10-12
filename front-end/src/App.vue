15af5c07af5c85b2246c6a0e2f972be929c89d76
hastyoops <hastyoops@gmail.com>
2021-10-12 14:44:12 +0200

    Added simple RoomList component

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <h1>Raspberry Home</h1>
+  <div>
+    <h1>Raspberry Home</h1>
+    <room-list></room-list>
+  </div>
 </template>
 
 <script>
+import RoomList from "./components/RoomList";
+
 export default {
   name: "App",
-  components: {},
+  components: { RoomList },
 };
 </script>
 
---
+++ front-end/src/components/RoomList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Rooms:</p>
+    <ul>
+      <li>Living Room</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RoomList",
+};
+</script>
