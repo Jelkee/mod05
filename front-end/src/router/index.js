fc0bcd3b80a34d8ace0361b488397fcefde51e06
Niek <n.zieverink@student.utwente.nl>
2021-10-19 12:19:30 +0200

    Added page where admin can create, edit and delete rooms.

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,15 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login.vue";
 import Dashboard from "../views/Dashboard.vue";
+import Components from "../views/Components.vue";
+import Rooms from "../views/Rooms.vue";
 
 const routes = [
-  { path: "/", name: "Login", component: Login },
+  { path: "/login", name: "Login", component: Login },
   { path: "/dashboard", name: "Dashboard", component: Dashboard },
   {
     path: "/dashboard/:id",
     name: "Room",
     component: Dashboard,
   },
+  { path: "/components", name: "Components", component: Components },
+  { path: "/rooms", name: "Rooms", component: Rooms },
 ];
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
